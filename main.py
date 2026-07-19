c435efd8bce885e944062682d915df2fd9ae410d
David Kohout <davidkohout08@gmail.com>
2026-07-19 16:49:33 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import is_char
-string = input("write a sentence ")
-list = []
-temp = ""
+import function
 
-for i in range(len(string)):
-    if i == (len(string) - 1):
-        if is_char.is_function(string[i]) == True:
-            temp += string[i]
-            break
-    if is_char.is_function(string[i]) == True:
-        temp += string[i]
-    elif is_char.is_function(string[i]) == False:
-        if string[i + 1] != " ":
-            temp += string[i]
-            
-        
+#separates words and stores them in a list
+sentence = input("what is your sentence? ")
+words = function.get_words(sentence)
 
-print(list)
-print(temp)
+#Remove punctuation from the beginning and end of each word
+for i in range(len(words)):
+    if not function.is_char(words[i][0]):
+        words[i] = words[i][1:]
+    if not function.is_char(words[i][-1]):
+        words[i] = words[i][:-1]
+
+#converts list into a string and prints
+final = ""
+for i in words:
+    final = final + i + " "
+
+print(f'{final} ')
